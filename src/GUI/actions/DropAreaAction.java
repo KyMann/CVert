7c926b905a5dfeb97de6db705315a7e70cfad2c2
Kyle <akavir7@gmail.com>
2017-04-24 17:24:32 -0500

    GUI skeleton
    drag n drop looks hard but needs to be done before the easier file conversion is done in the actions

---
+++ src/GUI/actions/DropAreaAction.java
@@ -0,0 +1,22 @@
+package GUI.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+/**
+ * Created by Kyle on 4/24/2017.
+ */
+public class DropAreaAction implements ActionListener, ItemListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
+    }
+}
